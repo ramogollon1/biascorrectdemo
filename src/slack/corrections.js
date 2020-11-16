7f53175e208ed9bfeaba1e97cb631daef23e7094
rafa <ramogollon1@ujap.edu.ve>
2020-11-15 23:53:00 -0300

    corrections

--- src/slack/corrections.js
+++ src/slack/corrections.js
@@ -1,6 +1,6 @@
 const biascorrect = require('../../lib/biascorrect/biascorrect')
 const biascorrect_views = require('../../lib/biascorrect/views/suggestion_message')
-const { randomValue } = require('./services')
+const {randomValue, randomValue2 } = require('./services')
 const suggestion_message = biascorrect_views.suggestion_message
 
 module.exports = {
@@ -8,13 +8,20 @@ module.exports = {
     const {text, user, channel} = event
     const props = {text, user, channel}
     const corrections = biascorrect.BIAS_CORRECTION(text)
-    const _correction = randomValue(corrections);
+    // console.log('corrections', corrections);
+    corrections.forEach((elem, i) => {
+      corrections[i] = randomValue2(corrections[i], text)
+    })
+    // console.log('corrections', corrections)
+    const _correction = randomValue(corrections[0]);
+    // console.log('_correction', _correction)
     if (!_correction.length || !corrections.length) return
     _correction.map( async correction => {
-      correction.TEXT = text
+      // correction.TEXT = text
       const mentionResponseBlock = { ...suggestion_message(correction), ...props}
-      await webClient.chat.postEphemeral(mentionResponseBlock)
+      const postEphemeral = await webClient.chat.postEphemeral(mentionResponseBlock)
     })
+    
     return _correction
   }
 }
